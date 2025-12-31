2efa44fadbce69866b8a0646400e87b8c5fdafcb
HassanIrfan527 <harryed525@gmail.com>
2025-12-31 17:52:50 +0500

    Create activity model and migration

---
+++ database/migrations/2025_12_31_124232_create_activities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('contact_id')->constrained()->onDelete('cascade'); // The "Who"
+            $table->string('description'); // e.g., "Order #101 was placed"
+
+            // These two columns allow the log to link to an Order, Invoice, or Product
+            $table->nullableMorphs('subject');
+
+            $table->json('properties')->nullable(); // Optional: Store old vs new values
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};
